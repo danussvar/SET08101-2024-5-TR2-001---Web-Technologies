3fc6103da3a23342c9b720e93f1eb1572cc03757
loisross <40650582@live.napier.ac.uk>
2025-04-22 16:09:25 +0100

    minor changes

--- scroll.js
+++ scroll.js
@@ -1,9 +1,6 @@
-// scroll.js
-
 document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.getElementById('start-button');
     startBtn.addEventListener('click', () => {
-      // Navigate to your first game page
       window.location.href = 'game.html';
     });
   });
