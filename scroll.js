afa71b1f1c2f7d774b2471f6bf24f6289035df95
loisross <40650582@live.napier.ac.uk>
2025-04-22 15:49:36 +0100

    finalised version

---
+++ scroll.js
@@ -0,0 +1,9 @@
+// scroll.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const startBtn = document.getElementById('start-button');
+    startBtn.addEventListener('click', () => {
+      // Navigate to your first game page
+      window.location.href = 'game.html';
+    });
+  });
